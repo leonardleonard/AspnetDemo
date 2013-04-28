dee6d97d499209a2dc225805b538c3c0bb2f27e9
leonardleonard <leonardleonard@126.com>
2013-04-28 09:05:45 +0800

    List<int> Split

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,12 +11,25 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             TestExcept();
+            //TestListint();
+        }
+
+        public static void TestListint()
+        {
+            string gameids = "1,2,3";
+            List<int> newGameIds = gameids.Split(',').Select(s => int.Parse(s)).ToList();
+
+            foreach (var item in newGameIds)
+            {
+                Console.WriteLine(item);
+            }
+
         }
 
         public static void TestExcept()
         {
 
-            int[] oldTag = new int[] { 1, 2, 3, 4 };
+            int[] oldTag = new int[] { 1, 2, 3 };
             int[] newTag = new int[] { 1, 2, 5 };
 
             var notExistsTags = oldTag.Except(newTag);
