087fe144054c760776508e4ceb71287046c51ff9
leonardleonard <leonardleonard@126.com>
2013-05-28 19:05:04 +0800

    DateDiff Add

---
+++ ConsoleApplication1/WebDemo1/DateDiff.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DateDiff.aspx.cs" Inherits="WebDemo1.DateDiff" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    2个时间的间隔，秒
+    </form>
+</body>
+</html>
---
+++ ConsoleApplication1/WebDemo1/DateDiff.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebDemo1
+{
+    public partial class DateDiff : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DateTime expiredTime = DateTime.Parse("2013-5-20 12:00:00");
+            DateTime createTime = DateTime.Parse("2013-5-18 13:00:00");
+
+            TimeSpan ts_expiredTime = new TimeSpan(expiredTime.Ticks); //获取当前时间的刻度数
+            TimeSpan ts_createTime = new TimeSpan(createTime.Ticks);
+
+            TimeSpan ts = ts_expiredTime.Subtract(ts_createTime).Duration(); //时间差的绝对值
+            int expiredSeconds = Convert.ToInt32(ts.TotalSeconds);
+
+            Response.Write(expiredSeconds.ToString());
+        }
+    }
+}
